a005e30a4de451c03fb5af971ecd07a6da291a01
Lara <laraimperial4@gmail.com>
2026-03-24 17:52:31 -0800

    Created sign up form and made improvements to navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
-//import Message from './Message';  
-import NavBar from './components/shared/NavBar';
-//import OrganizationAuthPage from './pages/organizations/OrganizationAuthPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/shared/NavBar";
+import OrganizationSignUpPage from "./pages/OrganizationSignUpPage";
+
+function Home() {
+  return <div></div>;
+}
+
+function Opportunities() {
+  return <div></div>;
+}
 
 function App() {
   return (
-      <div>
-         <NavBar  /> 
-      </div>
-   
+    <BrowserRouter>
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/opportunities" element={<Opportunities />} />
+        <Route path="/signin" element={<OrganizationSignUpPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
