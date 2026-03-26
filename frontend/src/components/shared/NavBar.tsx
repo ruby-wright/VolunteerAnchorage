18abd39bb8cdad8f2efea2c6a3e261117b07fd31
Lara <laraimperial4@gmail.com>
2026-03-25 19:36:09 -0800

    Clean up navbar merge conflicts

--- frontend/src/components/shared/NavBar.tsx
+++ frontend/src/components/shared/NavBar.tsx
@@ -46,17 +46,7 @@ function NavBar() {
               Search
             </button>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Opportunities</a>
-                </li>
-            </ul>
             <form className="d-flex" role="search">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button type="button" className="btn btn-outline-primary">Search</button>
             </form>
             </div>
           </form>
