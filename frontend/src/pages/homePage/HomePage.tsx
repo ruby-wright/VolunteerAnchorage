946c48a80add265c6e451cb7025005aaf59ac093
ggkwonn <wlgprnjs119@gmail.com>
2026-03-29 12:56:01 -0800

    Add HomePage component and associated CSS for volunteer opportunities

---
+++ frontend/src/pages/homePage/HomePage.tsx
@@ -0,0 +1,221 @@
+import React from "react";
+
+function HomePage() {
+    return (
+    <main role="main">
+        <section className="jumbotron text-center">
+            <div className="container">
+            <h1 className="jumbotron-heading">
+                Find Your Cause in Anchorage
+            </h1>
+            <p className="lead text-muted">
+                Discover local colunteer opportunities that match your interests and schedule. Make a difference in out community!
+            </p>
+            <p>
+                <a href="#" className="btn btn-primary my-2"> {/*Link to Advance Search Page*/}
+                Browse All Opportunies
+                </a>
+            </p>
+            </div>
+        </section>
+        <div className="album py-5 bg-light">
+            <div className="container">
+            <h6> Explore Volunteer Opportunites </h6>
+            <div className="row">
+                <div className="col-md-4">
+                <div className="card mb-4 box-shadow">
+                    <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap" />
+                    <div className="card-body">
+                    <p className="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="btn-group">
+                        <button type="button" className="btn btn-sm btn-outline-secondary">
+                            View Details
+                        </button>
+                         
+                        </div>
+                        <small className="text-muted">
+                        9 mins
+                        </small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div className="col-md-4">
+                <div className="card mb-4 box-shadow">
+                    <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap" />
+                    <div className="card-body">
+                    <p className="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="btn-group">
+                        <button type="button" className="btn btn-sm btn-outline-secondary">
+                            View Details
+                        </button>
+                         
+                        </div>
+                        <small className="text-muted">
+                        9 mins
+                        </small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div className="col-md-4">
+                <div className="card mb-4 box-shadow">
+                    <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap" />
+                    <div className="card-body">
+                    <p className="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="btn-group">
+                        <button type="button" className="btn btn-sm btn-outline-secondary">
+                            View Details
+                        </button>
+                         
+                        </div>
+                        <small className="text-muted">
+                        9 mins
+                        </small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div className="col-md-4">
+                <div className="card mb-4 box-shadow">
+                    <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap" />
+                    <div className="card-body">
+                    <p className="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="btn-group">
+                        <button type="button" className="btn btn-sm btn-outline-secondary">
+                            View Details
+                        </button>
+                         
+                        </div>
+                        <small className="text-muted">
+                        9 mins
+                        </small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div className="col-md-4">
+                <div className="card mb-4 box-shadow">
+                    <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap" />
+                    <div className="card-body">
+                    <p className="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="btn-group">
+                        <button type="button" className="btn btn-sm btn-outline-secondary">
+                            View Details
+                        </button>
+                         
+                        </div>
+                        <small className="text-muted">
+                        9 mins
+                        </small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div className="col-md-4">
+                <div className="card mb-4 box-shadow">
+                    <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap" />
+                    <div className="card-body">
+                    <p className="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="btn-group">
+                        <button type="button" className="btn btn-sm btn-outline-secondary">
+                            View Details
+                        </button>
+                         
+                        </div>
+                        <small className="text-muted">
+                        9 mins
+                        </small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div className="col-md-4">
+                <div className="card mb-4 box-shadow">
+                    <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap" />
+                    <div className="card-body">
+                    <p className="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="btn-group">
+                        <button type="button" className="btn btn-sm btn-outline-secondary">
+                            View Details
+                        </button>
+                         
+                        </div>
+                        <small className="text-muted">
+                        9 mins
+                        </small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div className="col-md-4">
+                <div className="card mb-4 box-shadow">
+                    <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap" />
+                    <div className="card-body">
+                    <p className="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="btn-group">
+                        <button type="button" className="btn btn-sm btn-outline-secondary">
+                            View Details
+                        </button>
+                         
+                        </div>
+                        <small className="text-muted">
+                        9 mins
+                        </small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div className="col-md-4">
+                <div className="card mb-4 box-shadow">
+                    <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap" />
+                    <div className="card-body">
+                    <p className="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="btn-group">
+                        <button type="button" className="btn btn-sm btn-outline-secondary">
+                            View Details
+                        </button>
+                         
+                        </div>
+                        <small className="text-muted">
+                        9 mins
+                        </small>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </main>
+    );  
+}
+
+export default HomePage;
